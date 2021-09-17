91f4189055d8006afa3b32450c95d90c77fbce12
Lou <dmitry.gumenyuk@gmail.com>
2021-09-17 10:55:19 +0200

    init

---
+++ src/components.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'design-web-components';
+
+
+
+export const UiBox_container = /*@__PURE__*/createReactComponent<JSX.UiBox_container, HTMLUiBox_containerElement>('ui-box_container');
+export const UiButton = /*@__PURE__*/createReactComponent<JSX.UiButton, HTMLUiButtonElement>('ui-button');
+export const UiCart_product = /*@__PURE__*/createReactComponent<JSX.UiCart_product, HTMLUiCart_productElement>('ui-cart_product');
+export const UiHeading = /*@__PURE__*/createReactComponent<JSX.UiHeading, HTMLUiHeadingElement>('ui-heading');
+export const UiImg = /*@__PURE__*/createReactComponent<JSX.UiImg, HTMLUiImgElement>('ui-img');
+export const UiLabel = /*@__PURE__*/createReactComponent<JSX.UiLabel, HTMLUiLabelElement>('ui-label');
+export const UiLink = /*@__PURE__*/createReactComponent<JSX.UiLink, HTMLUiLinkElement>('ui-link');
+export const UiModal = /*@__PURE__*/createReactComponent<JSX.UiModal, HTMLUiModalElement>('ui-modal');
+export const UiProduct_card = /*@__PURE__*/createReactComponent<JSX.UiProduct_card, HTMLUiProduct_cardElement>('ui-product_card');
+export const UiTextbox = /*@__PURE__*/createReactComponent<JSX.UiTextbox, HTMLUiTextboxElement>('ui-textbox');
+export const UiTrendingBlock = /*@__PURE__*/createReactComponent<JSX.UiTrendingBlock, HTMLUiTrendingBlockElement>('ui-trending-block');
